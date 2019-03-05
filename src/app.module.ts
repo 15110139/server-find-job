2a9a60ad41cafdbe91c578c83dc0e73bc06fa4cd
tienbmdev97 <tienbmdev97@gmail.com>
2019-03-06 00:49:41 +0700

    init

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+	imports: [ TypeOrmModule.forRootAsync({
+    useFactory:()=>({
+      type:'postgres',
+      host:'localhost',
+      port:5432,
+      username: 'postgres',
+      password: 'admin123',
+      database:'find-job',
+      entities: [ 'src/module/**/**/*.entity.ts' ],
+      synchronize:true,
+    })
+  }) ],
+	controllers: [ AppController ],
+	providers: [ AppService ]
+})
+export class AppModule {}
